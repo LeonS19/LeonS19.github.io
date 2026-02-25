7e110564d1ad03c728eb6b42eb2d0fd1e99b2c43
Leon Schäfer <leon.schaefer@student.hs-rm.de>
2026-02-25 21:31:29 +0100

    overview verlinkung gefixt, neues Projekt vorbereitet, slider für projekt 2 gemacht

--- script/i18n/translation.js
+++ script/i18n/translation.js
@@ -9,6 +9,11 @@ const translations = {
         milefizInfo: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
         doenerHeader: "Dönerverleih",
         doenerInfo: "Lorem Ipsum",
+
+        aekiHeader: "Aeki Raumplaner",
+        aekiInfo:"",
+        calumetHeader: "Calumet Shop Seite",
+        calumetInfo: "",
     },
     en: {
         projectOverview: "Project Overview",
@@ -20,6 +25,12 @@ const translations = {
         milefizInfo: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
         doenerHeader: "Kebab Rental",
         doenerInfo: "Lorem Ipsum",
+
+        aekiHeader:"Aeki spacial Planner",
+        aekiInfo:"",
+        calumetHeader: "Calumet Shop Site",
+        calumetInfo: "",
+
     }
 };
 
