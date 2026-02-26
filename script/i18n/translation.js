e2f975a2253d8a18534bd8bda4558744bf678761
Leon Schaefer <leon.schaefer@student.hs-rm.de>
2026-02-26 21:48:48 +0100

    projekt übersicht angepasst, bei den meisten projekten, video / cummy oder slider eingefügt

--- script/i18n/translation.js
+++ script/i18n/translation.js
@@ -6,14 +6,46 @@ const translations = {
         pageProjects: "Projekte",
         pageAbout: "Über Mich",
         pageContact: "Kontakt",
+        
+        milefizHeader: "MI'lefiz Meeplemania",
+        milefizSemesterInfo: "5. Semester – Softwaretechnik-Projekt",
         milefizInfo: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+        
         doenerHeader: "Dönerverleih",
+        doenerSemesterInfo: "4. Semester – Webbasierte Anwendung",
         doenerInfo: "Lorem Ipsum",
 
-        aekiHeader: "Aeki Raumplaner",
+        melodyEscapeHeader: "MelodyEscape",
+        melodyEscapeSemesterInfo: "3. Semester – Entwicklung Interaktiver Anwendungen",
+        melodyEscapeInfo: "",
+
+        lernwolkeHeader: "Lernwolke",
+        lernwolkeSemesterInfo: "5. Semester – Mensch-Computer-Interaktion",
+        lernwolkeInfo: "",
+
+        HKHeader: "Hollow Knight Animation",
+        HKSemesterInfo: "4. Semester – Motion Design",
+        HKInfo: "",
+
+        basicallHeader: "Basicall",
+        basicallSemesterInfo: "2. Semester – Interaction Design",
+        basicallInfo: "1. Im Vollbild anschauen 2. beim hover über produkt kann nicht gescrollt werden also sicher stellen, dass man beim scrollen nicht über ein image hovert 3. kleine Verlinkung auf Smartphone view",
+
+        mp3PlayerHeader: "MP3 Player",
+        mp3PlayerSemesterInfo: "3. Semester – Entwicklung Interaktiver Anwendung",
+        mp3PlayerInfo: "",
+
+        bookletHeader: "Booklet",
+        bookletSemesterInfo: "1. Semester – Grundlagen Mediendesign",
+        bookletInfo: "",
+
+        aekiHeader: "Schöner Wohnen – Raumplaner",
+        aekiSemesterInfo: "4. Semester – Softwaretechnik",
         aekiInfo:"",
+        
         calumetHeader: "Calumet Shop Seite",
-        calumetInfo: "",
+        calumetSemesterInfo: "2. Semester – Interaction Design",
+        calumetInfo: "1. Im Vollbild anschauen 2. beim hover über produkt kann nicht gescrollt werden also sicher stellen, dass man beim scrollen nicht über ein image hovert 3. kleine Verlinkung auf Smartphone view",
     },
     en: {
         projectOverview: "Project Overview",
@@ -22,14 +54,47 @@ const translations = {
         pageProjects: "Projects",
         pageAbout: "About",
         pageContact: "Contact",
+        
+        milefizHeader: "MI'lefiz Meeplemania",
+        milefizSemesterInfo: "5th Semester – Software Engineering Project",
         milefizInfo: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+        
         doenerHeader: "Kebab Rental",
+        doenerSemesterInfo: "4th Semester – Web-Based Application",
         doenerInfo: "Lorem Ipsum",
 
-        aekiHeader:"Aeki spacial Planner",
+        melodyEscapeHeader: "MelodyEscape",
+        melodyEscapeSemesterInfo: "3rd Semester – Interactive Application Development",
+        melodyEscapeInfo: "",
+
+        lernwolkeHeader: "Lernwolke",
+        lernwolkeSemesterInfo: "5th Semester – Human-Computer-Interaction",
+        lernwolkeInfo: "",
+
+        HKHeader: "Hollow Knight Animation",
+        HKSemesterInfo: "4th Semester – Motion Design",
+        HKInfo: "",
+
+        basicallHeader: "Basicall",
+        basicallSemesterInfo: "2nd Semester – Interaction Design",
+        basicallInfo: "1. Im Vollbild anschauen 2. beim hover über produkt kann nicht gescrollt werden also sicher stellen, dass man beim scrollen nicht über ein image hovert 3. kleine Verlinkung auf Smartphone view",
+
+        mp3PlayerHeader: "MP3 Player",
+        mp3PlayerSemesterInfo: "3rd Semester – Interactive Application Development",
+        mp3PlayerInfo: "",
+
+        bookletHeader: "Booklet",
+        bookletSemesterInfo: "1st Semester – Media Design Fundamentals",
+        bookletInfo: "",
+
+        aekiHeader: "Schöner Wohnen – Room Planner",
+        aekiSemesterInfo: "4th Semester – Software Engineering",
         aekiInfo:"",
-        calumetHeader: "Calumet Shop Site",
-        calumetInfo: "",
+        
+        calumetHeader: "Calumet Shop Website",
+        calumetSemesterInfo: "2nd Semester – Interaction Design",
+        calumetInfo: "1. Im Vollbild anschauen 2. beim hover über produkt kann nicht gescrollt werden also sicher stellen, dass man beim scrollen nicht über ein image hovert 3. kleine Verlinkung auf Smartphone view",
+
 
     }
 };
